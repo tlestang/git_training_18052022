7297ddfb28dad172d7e71aa962a006b78adb0028
Thibault Lestang <t.lestang@imperial.ac.uk>
2022-05-18 15:06:58 +0100

    Fit raw data using scipy.curve_fit

--- fit_data.py
+++ fit_data.py
@@ -1,9 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 filename = "example_data.csv"
 data = np.loadtxt(filename, delimiter=",", skiprows=1)
 plt.plot(data[:,0], data[:,1])
+
+def model(x, p1, p2):
+   return p1 * np.cos(p2 *x) + p2 * np.sin(p1 * x)
+
+popt, pcov = curve_fit(model, data[:,0], data[:,1], p0=(1, 0.2))
+
+x = np.linspace(-2, 2.9, 100)
+plt.plot(x, model(x, *popt))
+
 plt.show()
+
 
 
