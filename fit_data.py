a94d0eb84594f355152f2d877345ae728759a2ce
Thibault Lestang <t.lestang@imperial.ac.uk>
2022-05-18 15:34:01 +0100

    silly commit

--- fit_data.py
+++ fit_data.py
@@ -4,7 +4,7 @@ from scipy.optimize import curve_fit
 
 filename = "example_data.csv"
 data = np.loadtxt(filename, delimiter=",", skiprows=1)
-plt.plot(data[:,0], data[:,1])
+plt.plot(data[:,0], data[:,1], "ro")
 
 def model(x, p1, p2):
    return p1 * np.cos(p2 *x) + p2 * np.sin(p1 * x)
@@ -13,8 +13,5 @@ popt, pcov = curve_fit(model, data[:,0], data[:,1], p0=(1, 0.2))
 
 x = np.linspace(-2, 2.9, 100)
 plt.plot(x, model(x, *popt))
-
-plt.show()
-
 
 
